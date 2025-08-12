e1f330705fbd02f36b5d62b3a277682aae1b1b63
Gabriel Silva <gabrielaugusto.ifpr2024@gmail.com>
2025-08-11 23:17:01 -0300

    Update cartas.php

--- cartas.php
+++ cartas.php
@@ -5,7 +5,6 @@ class Carta {
     private string $nome;
     private string $tipo;
 
-
     public function getNumero(): int
     {
         return $this->numero;
@@ -33,11 +32,33 @@ class Carta {
 
 //Programa Principal
 $cartas = array();
-for ($i=0; $i < 7; $i++) { 
+for ($i=0; $i < 2; $i++) { 
     $carta = new Carta;
     $carta->setNumero(readline("Informe o número da carta: "));
     $carta->setNome(readline("Informe o nome da carta: "));
 
     $cartas[$i] = $carta;
 }
+
+
+$cartaSorteada = array_rand($cartas);
+
+do {
+    print "\nSuas cartas são: \n";
+
+    foreach ($cartas as $c) {
+        print "Número: " . $c->getNumero() . " | Nome: " . $c->getNome() . "\n";
+    }
+
+    $opcao = readline("Diga o número da carta que deseja chutar: ");
+
+    if ($opcao == $cartas[$cartaSorteada]->getNumero())  {
+        print "Você é bom hein\n";
+    } else {
+        print "Você é muito ruim, tente novamente\n";
+    }
+
+} while ($opcao != $cartas[$cartaSorteada]->getNumero());
+
+
 
