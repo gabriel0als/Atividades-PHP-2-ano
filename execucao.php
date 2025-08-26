41cb598b005dd7f9462be802c333d1e0c667f106
Gabriel Silva <gabrielaugusto.ifpr2024@gmail.com>
2025-08-26 11:56:30 -0300

    Add files via upload

--- execucao.php
+++ execucao.php
@@ -1,95 +1,72 @@
 <?php
 
-    require_once("modelo/Carro.php");
-    require_once("modelo/Fabricante.php");
+require_once("modelo/Pedido.php");
+require_once("modelo/Prato.php");
 
-    $matriz = [
-        ["nome" => "Volkswagem", "sigla" => "VW"],
-        ["nome" => "Chevrolet", "sigla" => "GM"],
-        ["nome" => "Fiat", "sigla" => "F"],
-        ["nome" => "Renault", "sigla" => "RN"]
-    ];
+$pedidos = array();
 
-    for ($i=0; $i < 4; $i++) { 
-         $fabricante = new Fabricante();
-         $fabricante->setNome($matriz[$i]["nome"]);
-         $fabricante->setSigla($matriz[$i]["sigla"]);
+$pratos = [
+    new Prato(1, "Camarão à Milanesa", 110.0),
+    new Prato(2, "Pizza Margherita", 80.0),
+    new Prato(3, "Macarrão à Carbonara", 60.0),
+    new Prato(4, "Bife à Parmegiana", 75.0),
+    new Prato(5, "Risoto ao Funghi", 75.0)
+];
 
-         $fabricantes[$i] = $fabricante;
+
+
+function retornaPrato($pratos, $numPrato) {
+    foreach ($pratos as $p) {
+        if ($numPrato == $p->getNumero()) {
+            return $p;
+        }
     }
 
+    return null;
+}
 
-$carros = array();
+do {
+    foreach ($pratos as $p) {
+        print "Número: " . $p->getNumero() . ", Nome: " . $p->getNome() . ", Valor: " . $p->getValor() . "\n";
+    }
 
-    do {
+    print "\n(1) - Cadastrar\n(2) - Cancelar\n(3) - Listar\n(4) - Total de vendas\n(0) - Sair\n";
 
-        print "(1) - Cadastrar carro\n(2) - Excluir carro\n(3) - Listar carros\n(0) - Sair\n\n";
+    $opcao = readline("Digite a ação que deseja: ");
+
+    switch ($opcao) {
+        case 1:
+            $pedido = new Pedido();
+            $pedido->setNomeCliente(readline("Informe o seu nome: "));
+            $pedido->setNomeGarcom(readline("Informe o nome do garçom: "));
+            $numPrato = readline("Informe o número do prato que deseja: ");
+
+            $numerosPratos = array();
+
+            $pedido->setPrato(retornaPrato($pratos, $numPrato));            
+
+            array_push($pedidos, $pedido);
+            break;
         
+        case 2:
 
-        $opcao = readline("Digite o número da ação: ");
+            break;
 
-        switch ($opcao) {
+        case 3:
 
-            case 1:
-                $carro = new Carro();
-                $carro->setModelo(readline("Informe o modelo do carro: "));
-                $carro->setAnoFabricacao(readline("Informe o ano de fabricação: "));
+            break;
 
-                print "Existem 4 fabricantes:\nVW (Volkswagem)\nGM (Chevrolet)\nF (Fiat)\nRN (Renault)\n";
+        case 4:
 
-                do {
-                        $opc = strtoupper(readline("\n\nDigite a sigla de um dos fabricantes: "));
-                        $i = 0;
+            break;
 
-                        if ($opc == "VW") {
-                        print "Boa opção!\n\n";
-                        $carro->setFabricante($fabricantes[0]);
-                        $i++;
-                    } elseif ($opc == "GM") {
-                        print "Boa opção!\n\n";
-                        $carro->setFabricante($fabricantes[1]);
-                        $i++;
-                    } elseif ($opc == "F") {
-                        print "Boa opção!\n\n";
-                        $carro->setFabricante($fabricantes[2]);
-                        $i++;
-                    } elseif ($opc == "RN") {
-                        print "Boa opção!\n\n";
-                        $carro->setFabricante($fabricantes[3]);
-                        $i++;
-                    } else {
-                        print "Essa opção não é válida\n";
-                    }
-                } while ($i != 1);
+        case 0:
+            print "Programa encerrado!";
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+} while ($opcao != 0);
 
-                array_push($carros, $carro);
-                break;
-            
-            case 2:
-                if (empty($carros)) {
-                    print "Você ainda não cadastrou nenhum carro\n\n";
-                    break;
-                }
-
-                foreach ($carros as $c) {
-                    print "Modelo: " . $c->getModelo() . " | Ano de fabricação: " . $c->getAnoFabricacao() . "\n";
-                }
-                
-                //TERMINAR AQUI
-
-
-                break;
-
-            case 3:
-
-                break;
-            
-            case 0:
-                print "Programa encerrado!";
-                break;
-
-            default:
-                print "Esse valor digitado não é válido\n";
-                break;
-        }
-    } while ($opcao != 0);
